b568741ff3fdc696c5f29ec0fc890f3252330c38
ChenSingYo <chensingyo@gmail.com>
2021-10-20 00:44:06 +0800

    add: hover 效果, 手機版nav

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ toTop.addEventListener('click', (e) => {
   e.preventDefault()
   header.scrollIntoView({ behavior: 'smooth', block: 'start' })
 })
+
+
+// 手機版彈出式選單＋按鈕
+
+const btnHamburger = document.querySelector('.icon_nav_mobile_ham')
+const btnClose = document.querySelector('.icon_nav_mobile_close')
+const mobileNav = document.querySelector('.btn_wrapper_mobile')
+
+btnHamburger.addEventListener('click', function(e) {
+  e.preventDefault()
+  btnHamburger.classList.remove('active')
+  mobileNav.classList.add('show_mobile_nav')
+  btnClose.classList.add('active')
+})
+
+btnClose.addEventListener('click', function(e) {
+  e.preventDefault()
+  btnHamburger.classList.add('active')
+  mobileNav.classList.remove('show_mobile_nav')
+  btnClose.classList.remove('active')
+})
+
